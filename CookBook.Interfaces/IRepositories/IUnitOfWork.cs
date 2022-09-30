6d70fced839a5d5024e7b77fefaa7f2ff097bb54
Rafał Huk <r.huk@outlook.com>
2022-09-30 21:10:36 +0200

    add repo interfaces

---
+++ CookBook.Interfaces/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Interfaces.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        IDishRepository Dishes { get; }
+        IMainProductRepository MainProducts { get; }
+        IUserRepository Users { get; }
+        Task SaveAsync();
+    }
+}
